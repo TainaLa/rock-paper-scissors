6c340472c103e2933dcb908c3e849cfe223e7d81
Taina Larosiliere <TainaC90@gmail.com>
2018-01-19 17:11:04 -0500

    fixed user and comp counter

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 var items = ['roc','pap','sciz']; //puts 3 choices in an array
+var userCount = 0;
+var computerCount = 0;
 
 // console.log(computerChoice);
 
@@ -21,27 +23,38 @@ function choice(event){
 }
 
 function winner(rps, computerChoice){
-
 	if(rps == computerChoice){
 		document.getElementById('msg').innerHTML = '<h2> Its a tie </h2>';
-		// console.log('???');
 	}
 	else if(rps == 'pap' && computerChoice == 'roc'){
+		userCount = userCount + 1;
 		document.getElementById('msg').innerHTML = '<h2> You win </h2>';
+		document.getElementById('you').innerText = userCount.toString();
 	}
 	else if(rps == 'pap' && computerChoice == 'sciz'){
+		computerCount += 1;
 		document.getElementById('msg').innerHTML = '<h2> You lose </h2>';
+		document.getElementById('comp').innerText = computerCount.toString();
+		
 	}
 	else if(rps == 'roc' && computerChoice == 'pap'){
+		computerCount += 1;
 		document.getElementById('msg').innerHTML = '<h2> You lose </h2>';
+		document.getElementById('comp').innerText = computerCount.toString();
 	}
 	else if(rps == 'roc' && computerChoice == 'sciz'){
+		userCount = userCount + 1;
 		document.getElementById('msg').innerHTML = '<h2> You win </h2>';
+		document.getElementById('you').innerText = userCount.toString();
 	}
 	else if(rps == 'sciz' && computerChoice == 'roc'){
+		computerCount += 1;
 		document.getElementById('msg').innerHTML = '<h2> You lose </h2>';
+		document.getElementById('comp').innerText = computerCount.toString();
 	}
 	else if(rps == 'sciz' && computerChoice == 'pap'){
+		userCount = userCount + 1;
 		document.getElementById('msg').innerHTML = '<h2> You win </h2>';
+		document.getElementById('you').innerText = userCount.toString();
 	}
 }
