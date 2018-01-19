1892f14806a15e7259418ac692188e4f97a27ca2
Taina Larosiliere <TainaC90@gmail.com>
2018-01-19 11:02:14 -0500

    moved var in choice function to generate a new computer choice

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
-var items = ['rock','paper','scissors']; //puts 3 choices in an array
-var computerChoice = items[Math.floor(Math.random()*items.length)];
+var items = ['roc','pap','sciz']; //puts 3 choices in an array
+
 // console.log(computerChoice);
 
 function choice(event){
+	var computerChoice = items[Math.floor(Math.random()*items.length)];
 	var rps = event.target.id;
 
 	if(rps == 'roc'){
@@ -20,26 +21,27 @@ function choice(event){
 }
 
 function winner(rps, computerChoice){
+
 	if(rps == computerChoice){
 		document.getElementById('msg').innerHTML = '<h2> Its a tie </h2>';
 		// console.log('???');
 	}
-	else if(rps == 'pap' && computerChoice == 'rock'){
+	else if(rps == 'pap' && computerChoice == 'roc'){
 		document.getElementById('msg').innerHTML = '<h2> You win </h2>';
 	}
-	else if(rps == 'pap' && computerChoice == 'scissors'){
+	else if(rps == 'pap' && computerChoice == 'sciz'){
 		document.getElementById('msg').innerHTML = '<h2> You lose </h2>';
 	}
-	else if(rps == 'roc' && computerChoice == 'paper'){
+	else if(rps == 'roc' && computerChoice == 'pap'){
 		document.getElementById('msg').innerHTML = '<h2> You lose </h2>';
 	}
-	else if(rps == 'roc' && computerChoice == 'scissors'){
+	else if(rps == 'roc' && computerChoice == 'sciz'){
 		document.getElementById('msg').innerHTML = '<h2> You win </h2>';
 	}
-	else if(rps == 'sciz' && computerChoice == 'rock'){
+	else if(rps == 'sciz' && computerChoice == 'roc'){
 		document.getElementById('msg').innerHTML = '<h2> You lose </h2>';
 	}
-	else if(rps == 'sciz' && computerChoice == 'paper'){
+	else if(rps == 'sciz' && computerChoice == 'pap'){
 		document.getElementById('msg').innerHTML = '<h2> You win </h2>';
 	}
 }
